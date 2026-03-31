06404251c2f28cde5710063c9315e28af887b0d2
holden.anderson <holden.anderson@students.snow.edu>
2026-03-31 16:19:48 -0600

    Threads notes

--- ConsoleApp13/Program.cs
+++ ConsoleApp13/Program.cs
@@ -1,35 +1,33 @@
 ﻿public class program
 {
-    static async Task Main()//nice VS code will change your void main to async Task main
+    //nice VS code will change your void main to async Task main
     // if you use wait inside the main method
+    static async Task Main()
     {
-        //Async Notes
-        //imagine making breakfast
+        // Async - threads
+        //Thread - how work is scheduled. How efficient is the worker.
+        //Async/await  The amount ouf workers
+    //    ? & or / ?
+        //runs code independently - concurrent work
 
-        //start the toast 
-        //wait for the toast to finish
-        //then starrt cooking eggs
-        //wait for eggs to finish
-        //start the steak
-        //wait for steak to finish
-
-        //using async solves this, you can do multiple
-        //things at the same time.
-        //it is often times used in websites so its not loading top to bottom
-
-        //async is not the same thing as threads, but they often
-        //walk hand in hand.
-
-//iwant to wait for all my thingsb ut  idont care wha order i get them in
-        Console.WriteLine("start breakfast!");
+        Thread t = new Thread(PrintNumbers);
+        t.Start();
         
-        Task steak = MakeSteak();
-        Task toast = MakeToast();
-        Task Eggs = MakeEggs(3);
+        Thread tt = new Thread(PrintNumbers);
+        tt.Start();
+        //i care more that you know what a race condition is than you trying to solve it, the code to solve it is so weird
+        //basically both of these threads look at the thing, and they dont wait their turn to look and changea 5 to a 6.
 
-        //key piece to make async really work right when all done. happens in the right order before program just dies
-        await Task.WhenAll(steak, toast, eggs);
-        Console.WriteLine("Breakfast is done");
+        Console.WriteLine("Main thead running...");
+
+    }
+    
+    private static void PrintNumbers()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            
+        }
     }
 
     public async Task MakeToast()
@@ -63,3 +61,21 @@
 
     }
 }
+
+
+  //Async Notes
+        //imagine making breakfast
+
+        //start the toast 
+        //wait for the toast to finish
+        //then starrt cooking eggs
+        //wait for eggs to finish
+        //start the steak
+        //wait for steak to finish
+
+        //using async solves this, you can do multiple
+        //things at the same time.
+        //it is often times used in websites so its not loading top to bottom
+
+        //async is not the same thing as threads, but they often
+        //walk hand in hand.
---
+++ ConsoleApp13/rawthreadnotes.cs
@@ -0,0 +1,22 @@
+﻿//create a raw thread, we dont do that often.
+//use task, and lock, rather than the low level method use lock to lock your different threads down 100% making them take turns for no overflow
+    //threads are very job specific, probably learn it for gaming. 
+    //Async you will use most everywhere, its good for you to load websites all at once not just 1 picture at a time.
+
+    //if a client says the program is slow, then you go and change it, threads are so fast so you could honestly make the program kinda slow but computers are so fast,
+    // the only bottleneck is going to and from the database.
+    //you just have to organize the database and retrieval methods i guess, you dont even have to make it nice, it could be rough organizing but that would still make it faster
+public class Program2
+{
+    static async Task Main()
+    {
+        Task task = Task.Run(() =>
+        {
+            for (int i = 0; i < 10; i ++)
+            {
+                Console.WriteLine($"Task: {i}");
+            }
+        }); //this looks funky because of the lambda, its all lining up though
+
+    }
+}
